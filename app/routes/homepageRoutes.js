ca8a6a6d992b6018999a85ecf37f23a21dd13fdf
Patrick <husspm@gmail.com>
2017-07-20 01:05:19 -0400

    cleaning up the folders

--- app/routes/homepageRoutes.js
+++ app/routes/homepageRoutes.js
@@ -7,17 +7,4 @@ module.exports = function(app) {
             res.render("home", { contents: data });
         });
     }); //home page route
-    app.get("/soundActivated", function(req, res) {
-        database.Item.findAll({}).then(function(data) {
-            res.render("soundActivated", { contents: data });
-        });
-    }); //sound page route
-
-    app.post("/create", function(req, res) {
-        database.Item.create({
-            name: req.body.name
-        }).then(function(data) {
-            res.redirect("/soundActivated");
-        });
-    });
 }; //ends exports function
