b84c42ffc72d7de3e58ae6be87a6331466f12087
Patrick <husspm@gmail.com>
2017-08-24 17:02:59 -0400

    Pushing up changes for mobile testing of looping mechanics

--- app/public/assets/js/test20.js
+++ app/public/assets/js/test20.js
@@ -2,8 +2,12 @@ function setup() {
     createCanvas(w - 20, h - 20);
 }
 var delay = new Tone.PingPongDelay(0.8, 0.8);
+var verb = new Tone.Freeverb(0.3);
+var delay2 = new Tone.PingPongDelay(0.4, 0.8);
+var verb2 = new Tone.Freeverb(0.9);
 var synth = new Tone.AMSynth({
     harmonicity: 4,
+    detune: 10,
     oscillator: {
         type: 'sawtooth4'
     },
@@ -16,18 +20,76 @@ var synth = new Tone.AMSynth({
     modulation: {
         type: 'sine'
     }
-}).chain(delay, Tone.Master);
+}).chain(delay, verb, Tone.Master);
+var synth2 = new Tone.AMSynth({
+    harmonicity: 4,
+    detune: 10,
+    oscillator: {
+        type: 'sawtooth4'
+    },
+    envelope: {
+        attack: 0.1,
+        decay: 0.2,
+        sustain: 0.5,
+        release: 0.1
+    },
+    modulation: {
+        type: 'square'
+    }
+}).chain(delay2, verb2, Tone.Master);
 
 var notes = [60, 62, 63, 65, 67, 68, 70, 72];
 var w = window.innerWidth;
 var h = window.innerHeight;
+var dots = [];
+
+//set the transport to repeat
+Tone.Transport.loopEnd = '4m';
+Tone.Transport.loop = true;
+Tone.Transport.start();
+
+function triggerSound(time) {
+    console.log('HIT');
+    var note = notes[Math.floor(random(notes.length))];
+    strokeWeight(200);
+    stroke(random(255), random(255), random(255), 10);
+    point(random(w), random(h));
+    synth2.triggerAttackRelease(midiToFreq(note), '16n', time);
+}
 
 function mousePressed() {
     var indexOfNote = Math.floor(map(mouseX, 0, w, 0, notes.length));
+    if (indexOfNote === -1) {
+        indexOfNote = 0;
+    }
+    console.log(indexOfNote);
     var noteToPlay = notes[indexOfNote];
-    strokeWeight(50);
-    stroke(255, 50);
-    point(mouseX, mouseY);
+    dot = new Dot(mouseX, mouseY, 250);
+    dots.push(dot);
     synth.harmonicity = random(2, 20);
     synth.triggerAttackRelease(midiToFreq(noteToPlay), '2n');
+    Tone.Transport.stop();
+    Tone.Transport.start();
+    var when = Tone.Transport.seconds.toFixed(2);
+    Tone.Transport.schedule(triggerSound, when);
+}
+
+function draw() {
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].show();
+        if (dots[i].initial < 10) {
+            dots.splice(i, 1);
+        }
+    }
+}
+
+function Dot(x, y, initial) {
+    this.x = x;
+    this.y = y;
+    this.initial = initial;
+    this.show = function() {
+        strokeWeight(50);
+        stroke(initial *= 0.99, initial *= 0.98, initial *= 0.96, 10);
+        point(this.x, this.y);
+    }
 }
