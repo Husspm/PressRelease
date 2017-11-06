65208c184b4c3ae910834bc6a61382c2ffd960aa
Patrick <husspm@gmail.com>
2017-11-06 13:05:25 -0500

    Changes to database

--- app/server.js
+++ app/server.js
@@ -19,7 +19,7 @@ app.use(method("_method"));
 
 
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
 
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
